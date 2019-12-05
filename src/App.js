eda2ca5a32412403de01e82ee2d1ac981a7dc33d
Wiklo, Tomasz (d31-EXTERN) <Tomasz.Wiklo.extern@itergo.com>
2019-12-05 16:12:35 +0100

    display 4 items per category on shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import HomePage from './pages/homepage/homepage.component';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
