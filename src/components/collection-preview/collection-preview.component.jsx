3010e3b53356a71232e4164f1cc191191bc76cea
Wiklo, Tomasz (d31-EXTERN) <Tomasz.Wiklo.extern@itergo.com>
2019-12-06 15:46:23 +0100

    create shop page with items listing

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import './preview-collection.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component'
+import './collection-preview.styles.scss';
 
 const CollectionPreview = ({title, items}) => (
     <div className="collection-preview">
@@ -8,8 +8,8 @@ const CollectionPreview = ({title, items}) => (
         <div className="preview">
             {items
             .filter((item, index) => index < 4)
-            .map(item => (
-                <div key={item.id}>{item.name}</div>
+            .map(({id, ...otherItemProps}) => (
+                <CollectionItem key={id} {...otherItemProps} />
             ))}
         </div>
     </div>
