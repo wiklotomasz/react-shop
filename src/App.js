b13c00486d4aa1009f563ca9a0f4e008d992e99b
Tomasz Wiklo <wikweb@gmail.com>
2019-12-09 12:47:42 +0100

    Add Login Form mockup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSingnUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSingnUpPage} />
       </Switch>
     </div>
   );
