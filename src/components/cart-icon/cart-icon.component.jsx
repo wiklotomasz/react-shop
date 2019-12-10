7a3e70404ece1b79a568aa45d8951c2fe78bcd6e
Tomasz Wiklo <wikweb@gmail.com>
2019-12-10 13:30:09 +0100

    add cart dropdown toggle

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
-import './cart-icon.styles.scss';
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-function mapStateToProps(state) {
-    return {
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import "./cart-icon.styles.scss";
 
-    };
-}
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
+    <ShoppingIcon className="shopping-icon" />
+    <span className="item-count">0</span>
+  </div>
+);
 
-class CartIcon extends Component {
-    render() {
-        return (
-            <div className='cart-icon'>
-                <ShoppingIcon className="shopping-icon" />
-                <span className="item-count">0</span>
-            </div>
-        );
-    }
-}
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+});
 
-export default connect(
-    mapStateToProps,
-)(CartIcon);
+export default connect(null, mapDispatchToProps)(CartIcon);
