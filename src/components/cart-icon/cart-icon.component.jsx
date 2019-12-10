ecbcdcee5e8a627001003e1ff61e1da02fea7470
Tomasz Wiklo <wikweb@gmail.com>
2019-12-10 12:57:38 +0100

    add shopping bag icon

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+import './cart-icon.styles.scss';
+
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+class CartIcon extends Component {
+    render() {
+        return (
+            <div className='cart-icon'>
+                <ShoppingIcon className="shopping-icon" />
+                <span className="item-count">0</span>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+)(CartIcon);
