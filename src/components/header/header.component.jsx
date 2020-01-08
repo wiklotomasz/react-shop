b5164a92c0df31131209a286649ee9818d3da43c
Tomasz Wiklo <wikweb@gmail.com>
2020-01-08 10:24:01 +0100

    convert scss to css-in-js

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,35 +11,35 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles';
 
 const Header = ({currentUser, hidden}) => {
     return (
-        <div className='header'>
-            <Link className='logo-container' to="/">
+        <HeaderContainer>
+            <LogoContainer to="/">
                 <Logo className='logo' />
-            </Link>
-            <div className="options">
-                <Link className="option" to='/shop'>
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to='/shop'>
                     SHOP
-                </Link>
-                <Link className="option" to='/shop'>
+                </OptionLink>
+                <OptionLink to='/shop'>
                     CONTACT
-                </Link>
+                </OptionLink>
                 {
                     currentUser ?
-                    <div className="option" onClick={()=> auth.signOut()}>SIGN OUT</div>
+                    <OptionDiv onClick={()=> auth.signOut()}>SIGN OUT</OptionDiv>
                     :
                     <Link className="option" to='/signin'>SIGN IN</Link>
                 }
                 <CartIcon />
-            </div>
+            </OptionsContainer>
             {
                 hidden ? null :
                 (<CartDropdown />)
             }
             
-        </div>
+        </HeaderContainer>
     );
 };
 
