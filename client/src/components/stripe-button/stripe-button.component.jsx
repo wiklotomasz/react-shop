b2c6c5390c598eaa719da5583827e92797da8811
Tomasz Wiklo <wikweb@gmail.com>
2020-03-23 14:21:50 +0100

    add BE to the project

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_daCgyOHKacth4GeK8ljJhRRo00CdPdF6Nm';
 
     const onToken = token => {
-        console.log(token);
-        alert("Payment sucessful");
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert("Payment sucessful");
+        }).catch(error => {
+            console.log('Payment error: ', error);
+            alert("Payment error. Please check if credit card provided is correct");
+        })
     }
 
     return (
--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -6,7 +6,6 @@ import CollectionItem from "../../components/collection-item/collection-item.com
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
-import { firestore } from 'firebase';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
