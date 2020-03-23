8bd3ac6060c2599bbf7819dfb377879c18d79f83
Tomasz Wiklo <wikweb@gmail.com>
2020-03-23 14:35:13 +0100

    change cors options

--- server.js
+++ server.js
@@ -13,7 +13,21 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(cors());
+var allowedOrigins = ['http://localhost:3000',
+                      'https://react-shoper.herokuapp.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
