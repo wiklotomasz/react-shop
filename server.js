5a804c165566615c333829bba57013e3382abd0e
Tomasz Wiklo <wikweb@gmail.com>
2020-05-05 15:19:24 +0200

    add compress for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compress = require('compress');
 
 if (process.env.NODE_ENV != 'production') require('dotenv').config();
 
@@ -10,6 +11,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compress());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
