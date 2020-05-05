df7e7197f17869b932589ff0f4459ec7aff4a666
Tomasz Wiklo <wikweb@gmail.com>
2020-05-05 15:26:22 +0200

    fix wrong addon in package

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const compress = require('compress');
+const compression = require('compression');
 
 if (process.env.NODE_ENV != 'production') require('dotenv').config();
 
@@ -11,7 +11,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compress());
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
